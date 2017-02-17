2954cc96bbb137b8e0faf8672e5c58df24c4cf37
Kish <kish@kishpatel.com>
2017-02-17 18:48:51 +0000

    Initial Commit

---
+++ AELottie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AELottie
+//
+//  Created by Kish Patel on 17/02/2017.
+//  Copyright © 2017 Kish Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
